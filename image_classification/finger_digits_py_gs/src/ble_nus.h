786c91d6d407c99d7b382225d0377dea8b500607
BigBoiBerry <nicholasjohanberryman@gmail.com>
2026-08-07 12:28:19 +0200

    LED turns on when connected AND central has subscribed

--- image_classification/finger_digits_py_gs/src/ble_nus.h
+++ image_classification/finger_digits_py_gs/src/ble_nus.h
@@ -1,5 +1,5 @@
 #include <zephyr/kernel.h>
 
-int init_ble_nus(void);
+int init_ble_nus(const struct gpio_dt_spec *led);
 bool ble_nus_ready(void);
 int ble_nus_send(const void *data, uint16_t len);
